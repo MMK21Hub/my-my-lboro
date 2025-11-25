30f7cb025a71f219b9543890ba70ff5637675654
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-11-25 20:06:40 +0000

    FastAPI go brr?

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 
+from fastapi import FastAPI
+
 from my_lboro import MyLboro
 from config import config
 
@@ -12,3 +14,10 @@ cal_start = datetime.now() + timedelta(days=0)
 cal_end = datetime.now() + timedelta(days=7)
 course_timetable = client.get_calendar_events("course_timetable", cal_start, cal_end)
 print(course_timetable)
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
