65bab2bdc83c38ed6dffdda0d18310d5432f00e6
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-11-25 20:06:22 +0000

    Add a LboroError class for better API error handling

--- my_lboro.py
+++ my_lboro.py
@@ -1,7 +1,31 @@
 from datetime import datetime
-from requests import Session
+from requests import Response, Session
 
 from my_lboro_types import CalendarEventsResponse
+
+
+class LboroError(Exception):
+    """An error returned by the myLboro API"""
+
+    def __init__(self, error_type: str, description: str | None = None):
+        self.error_type = error_type
+        self.description = description
+        super().__init__(f"Error from myLboro ({error_type}): {description}")
+
+
+def get_error_data(response: Response) -> LboroError | None:
+    """If we can recognise the structure of the given error response, parse it into a LboroError. Else return None."""
+    try:
+        data = response.json()
+    except ValueError:
+        return None
+    if type(data) is not dict:
+        return None
+    error_type = data.get("type")
+    if error_type is None:
+        return None
+    error_description = data.get("desc")
+    return LboroError(error_type, error_description)
 
 
 class MyLboro:
@@ -35,6 +59,10 @@ class MyLboro:
             },
             timeout=10,
         )
+        if res.status_code == 500:
+            error = get_error_data(res)
+            if error:
+                raise error
         res.raise_for_status()
         account_info = res.json()
         return account_info
