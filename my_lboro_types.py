93825d955471bfa4c58b237e650062ff8b3c84ed
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-09-07 14:06:41 +0100

    Add support for comma-separated UUID lists

--- my_lboro_types.py
+++ my_lboro_types.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 from datetime import datetime
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, BeforeValidator, Field
 
 
 class Event(BaseModel):
@@ -16,7 +19,7 @@ class Event(BaseModel):
     is_all_day: bool = Field(alias="isAllDay")
     duration_unit: str | None = Field(alias="durationUnit")
     legend_col: str | None = Field(alias="legendCol")
-    loc_code: UUID = Field(alias="locCode")
+    loc_code: CommaSeparatedList[UUID] = Field(alias="locCode")
     loc_add1: str = Field(alias="locAdd1")
     loc_add2: str | None = Field(alias="locAdd2")
     loc_add3: str | None = Field(alias="locAdd3")
@@ -26,10 +29,19 @@ class Event(BaseModel):
     loc_post_code: str | None = Field(alias="locPostCode")
     attendance_exclude: bool = Field(alias="attendanceExclude")
     meeting: bool
-    meeting_url: str | None = Field(alias="meetingUrl")
+    meeting_url: str | None = Field(alias="meetingUrl", default=None)
     teacher_name: str | None = Field(alias="teacherName", default=None)
     teacher_email: str | None = Field(alias="teacherEmail", default=None)
 
 
 class CalendarEventsResponse(BaseModel):
     events: list[Event]
+
+
+def csv_to_list(value: str) -> list[str]:
+    if isinstance(value, str):
+        return value.split(", ")
+    raise ValueError("Not a comma-separated string")
+
+
+type CommaSeparatedList[T] = Annotated[list[T], BeforeValidator(csv_to_list)]
