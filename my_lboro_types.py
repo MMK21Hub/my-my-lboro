abdb79c13688a30f719cb52e15f70765ee8a7fb7
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-09-07 13:23:57 +0100

    Update Event model with new fields
    And convert `Optional[]` to `| None`

--- my_lboro_types.py
+++ my_lboro_types.py
@@ -1,21 +1,34 @@
-from pydantic import BaseModel, Field
 from datetime import datetime
 from uuid import UUID
-from typing import Optional
+
+from pydantic import BaseModel, Field
 
 
 class Event(BaseModel):
     event_ref: str = Field(alias="eventRef")
     desc1: str
     desc2: str
+    desc3: str
+    alert_com: str = Field(alias="alertCom")  # ??
     cal_date: datetime = Field(alias="calDate")
     start: datetime
     end: datetime
+    is_all_day: bool = Field(alias="isAllDay")
+    duration_unit: str | None = Field(alias="durationUnit")
+    legend_col: str | None = Field(alias="legendCol")
     loc_code: UUID = Field(alias="locCode")
     loc_add1: str = Field(alias="locAdd1")
+    loc_add2: str | None = Field(alias="locAdd2")
+    loc_add3: str | None = Field(alias="locAdd3")
+    loc_add4: str | None = Field(alias="locAdd4")
+    loc_add_post_code: str | None = Field(alias="locAddPostCode")
+    loc_work_tel: str | None = Field(alias="locWorkTel")
+    loc_post_code: str | None = Field(alias="locPostCode")
     attendance_exclude: bool = Field(alias="attendanceExclude")
-    teacher_name: Optional[str] = Field(alias="teacherName", default=None)
-    teacher_email: Optional[str] = Field(alias="teacherEmail", default=None)
+    meeting: bool
+    meeting_url: str | None = Field(alias="meetingUrl")
+    teacher_name: str | None = Field(alias="teacherName", default=None)
+    teacher_email: str | None = Field(alias="teacherEmail", default=None)
 
 
 class CalendarEventsResponse(BaseModel):
