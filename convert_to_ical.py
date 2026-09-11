f34fddb1f8810652d2555986d45d30d916e5f67a
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2026-09-11 20:15:40 +0100

    Include type in title

--- convert_to_ical.py
+++ convert_to_ical.py
@@ -9,14 +9,15 @@ def convert_events_to_ical(events: list[LboroEvent]) -> Iterable[str]:
     calendar = Calendar()
     for lboro_event in events:
         description = [
+            f"Module: {lboro_event.desc1} - {lboro_event.desc2}",
             f"Type: {lboro_event.desc3}",
             f"Lecturer: {lboro_event.teacher_name}",
         ]
 
         calendar.events.add(
             Event(
-                # desc1 is module code, desc2 is module name
-                name=f"{lboro_event.desc1}: {lboro_event.desc2}",
+                # desc1 is module code, desc2 is module name, desc3 is event type
+                name=f"{lboro_event.desc1}: {lboro_event.desc2} ({lboro_event.desc3})",
                 begin=lboro_event.start,
                 end=lboro_event.end,
                 location=lboro_event.loc_add1,
